3fef65eeb0689857a787b43efc2dafaae4cf4320
shairez <psyrez@gmail.com>
2015-09-07 11:20:06 +0300

    added commonjs support

---
+++ index.js
@@ -0,0 +1,8 @@
+require('angular');
+require('angular-ui-router');
+require('angular-local-storage');
+
+require('./dist/angular-secured');
+require('./dist/angular-secured-loopback');
+
+module.exports = 'ngSecured';

8d422072aae9fbd01730a7fd387015fe9140e4ad
shairez <psyrez@gmail.com>
2015-10-16 14:01:00 +0300

    added path prefix to localstorge

--- dist/angular-secured.js
+++ dist/angular-secured.js
@@ -180,7 +180,8 @@
                     $httpProvider,
                     authAdapterProvider,
                     permissionsDaoProvider,
-                    securityEnforcerProvider) {
+                    securityEnforcerProvider,
+                    $windowProvider) {
 
     var config = {
       adapterOptions: {},
@@ -199,11 +200,16 @@
 
     this.secure = function (userConfig) {
       angular.extend(config, userConfig);
-      localStorageServiceProvider.setPrefix('ngSecured');
+      localStorageServiceProvider.setPrefix('ngSecured_'+ getAppPath() + '_');
       //authAdapterProvider.setup(config.adapterOptions);
       permissionsDaoProvider.setup(config.permissions);
       securityEnforcerProvider.setupPages(config.pages);
     };
+
+    function getAppPath(){
+      var $window = $windowProvider.$get();
+      return $window.location.pathname;
+    }
 
     this.$get = factory;
 
